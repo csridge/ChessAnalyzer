c34503e351d06a78fa2c1b226203d8495a2cb434
John Ridge <johnridgevn@gmail.com>
2024-06-17 21:57:39 +0700

    Update main.js and index.html

--- js/main.js
+++ js/main.js
@@ -3,28 +3,33 @@ let cpuCount = 0;
 let cpuCost = 10;
 let pps = 0;
 let multiplier = 1;
+let cpuRequires = 10;
 function updateUI(){
     document.getElementById('positions').textContent = positions;
     document.getElementById('cpuCost').textContent = cpuCost;
     document.getElementById('cpuCount').textContent = cpuCount
     document.getElementById('pps').textContent=pps;
     document.getElementById('mult').textContent=multiplier;
+    document.getElementById('cpuUpdtReq').textContent=cpuRequires;
 }
 function buyCPU(){
-    if (positions >= cpuCost){
-        positions - cpuCost;
-        cpuCost = Math.round(0.5*(cpuCost**1.15)+15)
-        pps+=Math.round(0.5*(pps**1.06)+4);
+    if(positions >= cpuCost){
+        positions -= cpuCost;
+        pps+=1;
         cpuCount+=1;
-    }
-    if(cpuCount%10==0&&cpuCount!=0){
-        document.getElementById('upgradeBtn').style.display="block";
     }
 }
 function upgrade(){
+    if(cpuCount%cpuRequires==0&&cpuCount!=0){
+        document.getElementById('upgradeBtn').classList.remove('unclickable');
         cpuCost=Math.round(cpuCost**1.25);
         positions=0;
         multiplier*=2;
+        cpuRequires=Math.round(cpuRequires**1.1);
+    }
+    else{
+        document.getElementById('upgradeBtn').classList.add('unclickable');
+    }
 }
 function increment(){
     positions+=pps*multiplier;
@@ -35,6 +40,6 @@ function onclick(){
 }
 setInterval(function gameLoop(){
     updateUI();
-    positions += pps;
-    buyCPUBtn.addEventListener('click', buyCPU, false);
+    increment();
+    onclick();
 },1000)
